seifferth/gemdoc

commit 5e3c788d47ce09a8d06a0525e029bf8bbcaa21df

Do not translate empty preformatted blocks into html
I. e. do not render anything in html if the preformatted block
contains exactly zero lines. If it contains a single empty line,
that line will still be rendered.

This is also a preparatory commit for introducing inline images.

gemdoc.py
@@ -459,7 +459,10 @@ def parse_gemini(doc: str, metadata: dict) -> tuple[str,str]:
         elif preformatted:
             add(doc[i], tag=None)
         elif doc[i].startswith('```'):
-            body.append('<pre>'); preformatted = True
+            if i+1 < len(doc) and doc[i+1].startswith('```'):
+                pass
+            else:
+                body.append('<pre>'); preformatted = True
         elif doc[i].startswith('###'):
             body.append('<div class="headingcontext">')
             add(doc[i][3:].strip(), tag='h3')

commit 9c6ab5b6a24d63eca73e7c0759cfbd43732d2462

Fix broken code block parsing introduced in commit 5e3c788d

gemdoc.py
@@ -460,7 +460,7 @@ def parse_gemini(doc: str, metadata: dict) -> tuple[str,str]:
             add(doc[i], tag=None)
         elif doc[i].startswith('```'):
             if i+1 < len(doc) and doc[i+1].startswith('```'):
-                pass
+                i += 1
             else:
                 body.append('<pre>'); preformatted = True
         elif doc[i].startswith('###'):